d7da3dd29989cb034aa9c992540b0f79796f0ed8
yayanyang <yayanyang@gmail.com>
2024-09-08 12:02:41 +0800

    update

--- crates/kad/tests/kad.rs
+++ crates/kad/tests/kad.rs
@@ -35,11 +35,11 @@ async fn init() -> Switch {
 
 #[futures_test::test]
 async fn find_node() {
-    use hala_pprof_memory::report::snapshot;
-    use hala_pprof_memory::PprofAlloc;
+    // use hala_pprof_memory::report::snapshot;
+    // use hala_pprof_memory::PprofAlloc;
 
-    #[global_allocator]
-    static ALLOC: PprofAlloc = PprofAlloc;
+    // #[global_allocator]
+    // static ALLOC: PprofAlloc = PprofAlloc;
 
     let switch = init().await;
 
@@ -85,7 +85,7 @@ async fn find_node() {
             transport_status,
         );
 
-        snapshot();
+        // snapshot();
     }
 }
 
